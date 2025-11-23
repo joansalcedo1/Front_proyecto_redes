cced0d6b1052363838116dac5fd162de236e5eae
joansalcedo1 <sebastiansalcedo7@gmail.com>
2025-11-23 13:31:45 -0500

    carpeta data simulada creada

--- services.js
+++ services.js
@@ -1,3 +1,5 @@
+const URL_BASE = "http://localhost:"
+const puertoConvocatoria = 3308
 function togglePasswordVisibility() {
     const passwordInput = document.getElementById('password');
     const imgIcon = document.getElementById('iconPassword');
@@ -6,4 +8,22 @@ function togglePasswordVisibility() {
     passwordInput.setAttribute('type', type);
     imgIcon.setAttribute('src', icon);
 
+}
+
+async function getAllconvocatorias() {
+    const res = fetch(`${URL_BASE}${puertoConvocatoria}/apiRedes/convocatoria`)
+    const convocatorias = await res.json();
+
+    const template = document.getElementById("convocatoria-template");
+    const grid = document.getElementById("convocatorias-grid");
+
+    convocatorias.array.forEach(item => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector("[data-title]").textContent = item.titulo;
+        clone.querySelector("[data-description]").textContent = item.descripcion;
+        clone.querySelector("[data-date]").textContent = `Fecha: ${item.fecha}`;
+
+        grid.appendChild(clone);
+
+    });
 }
